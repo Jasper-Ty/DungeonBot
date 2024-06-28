a95356a11434f4e7a582cebd4e2fd6315ea591e8
Jasper Ty <jasperty2@gmail.com>
2024-06-28 18:44:06 -0400

    Implement serenity example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::fs;
 
-fn main() {
+use toml::Table;
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::prelude::*;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "pong!").await {
+                println!("Error sending message: {why:?}");
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let secrets = fs::read_to_string("secrets.toml")
+        .as_deref()
+        .map(str::parse::<Table>)
+        .unwrap()
+        .unwrap();
+
+    let bot_token = secrets["bot_token"].as_str().unwrap();
+
+    let intents = GatewayIntents::GUILD_MESSAGES 
+        | GatewayIntents::DIRECT_MESSAGES 
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = 
+        Client::builder(&bot_token, intents).event_handler(Handler).await.unwrap();
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {why:?}");
+    }
 }
