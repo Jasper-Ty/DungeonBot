251831739e4b99968f544f85d3cfd1938401ea77
Jasper Ty <jasperty2@gmail.com>
2024-07-15 15:56:13 -0400

    Update src/db/schema.rs

--- src/db/schema.rs
+++ src/db/schema.rs
@@ -1,8 +1,20 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    state (key) {
+        key -> Nullable<Text>,
+        value -> Text,
+    }
+}
+
+diesel::table! {
     users (id) {
         id -> BigInt,
         points -> Integer,
     }
 }
+
+diesel::allow_tables_to_appear_in_same_query!(
+    state,
+    users,
+);
